00f98618a779489035076a2e3e22658880453da6
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-19 19:22:01 -0300

    Update hellfire-club.js

--- Mundo-Invertido-com-Fire-base/assets/js/firebase/hellfire-club.js
+++ Mundo-Invertido-com-Fire-base/assets/js/firebase/hellfire-club.js
@@ -1,9 +1,10 @@
 import app from './firebase/app.js'
 import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js'
 
-export function subscribeToHellfireClub(subscription) {
+export async function subscribeToHellfireClub(subscription) {
     const db = getFirestore(app)
     const hellfireClubCollection =  collection(db, 'hellfire-clube')
     addDoc(hellfireClubCollection, subscription)
-
+    const docRef = await addDoc(hellfireClubCollection, subscription)
+    return docRef.id
 }
