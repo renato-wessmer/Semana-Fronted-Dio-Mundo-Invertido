a8cc90298157e5aa43412ac2fe687be7c2be0de8
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-19 17:25:51 -0300

    Create app.js

---
+++ Mundo-Invertido-com-Fire-base/assets/js/firebase/app.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBPNUMF5e7Tn42TLd-Glt8SxZAqVdgttmE",
+    authDomain: "semana-invertido.firebaseapp.com",
+    projectId: "semana-invertido",
+    storageBucket: "semana-invertido.appspot.com",
+    messagingSenderId: "210337645645",
+    appId: "1:210337645645:web:758629bc0c634016154804"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
+
+
