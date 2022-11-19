396ef1f5d261eb7e737b719b3256fb171dcf88ef
Renato Wéssmėr <rewesa@outlook.com.br>
2022-11-19 17:25:53 -0300

    Create main.js

---
+++ Mundo-Invertido-com-Fire-base/assets/js/main.js
@@ -0,0 +1,28 @@
+import app from './firebase/app.js'
+import { subscribeToHellfireClub } from './firebase/hellfire-club.js'
+
+console.log(app)
+
+const txtName = document.getElementById('txtName')
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+
+const btnSubscribe = document.getElementById('btnSubscribe')
+
+btnSubscribe.addEventListener('click', () => {
+    const subscription = {
+        name: txtName.valeu,
+        email: txtEmail.value,
+        level: txtLevel.value, 
+        character: txtCharacter.value 
+    }
+
+    // Save in Database
+    subscribeToHellfireClub(subscription)
+
+})    
+
+
+
+
